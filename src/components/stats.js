9e23184e803b60820283145a15ccffc81c685d58
unknown <Joseph.H.Stoops@GMail.com>
2018-08-23 09:02:07 -0600

    Added the raw conversion % columns as requested. Also added title attributes to several html tags to show more information about the columns and components of the page.

--- src/components/stats.js
+++ src/components/stats.js
@@ -7,76 +7,100 @@ class Stats extends Component {
         this.state = {
             loggedId: this.props.getLoggedIn,
             siteGoal: {
-                'Aetna': 200,
-                'Anthem': 175,
-                'CareSource': 120
+                'Aetna': {
+                    'Provo': 250,
+                    'Salt Lake City': 350,
+                    'Sawgrass': 215,
+                    'San Antonio': 180,
+                    'Memphis': 170
+                },
+                'Anthem': {
+                    'San Antonio': 120,
+                    'Roy': 100,
+                    'Sawgrass': 150
+                },
+                'CareSource': {
+                    'Salt Lake City': 120
+                }
             }
         };
     }
+
+    enrollments(stats, product, callerType){
+        if (!stats) return 0;
+        const result = stats.reduce((acc, call) => Number(call.product === product && call.callerType === callerType && call.enrollment === 1) + acc, 0);
+        if (result === null || isNaN(result) || result === undefined) return 0;
+        return result;
+    }
+
+    conversionRate(stats, product){
+        if (!stats) return 0;
+        const result = Number(stats.reduce((acc, call) => Number(call.product === product && call.callerType === 'P' && call.enrollment) + acc, 0) /
+            stats.reduce((acc, call) => Number(call.product === product && call.callerType === 'P' && call.opportunity === 1) + acc, 0) * 100).toFixed(2)
+        if (result === null || isNaN(result) || result === undefined) return 0;
+        return result;
+    }
+
+    rawConversionRate(stats, product){
+        if (!stats) return 0;
+        const result = Number(stats.reduce((acc, call) => Number(call.product === product && (call.hv === 1 || call.lacb || call.enrollment)) + acc, 0) /
+            stats.reduce((acc, call) => Number(call.product === product && call.opportunity === 1) + acc, 0) * 100).toFixed(2)
+        if (result === null || isNaN(result) || result === undefined) return 0;
+        return result;
+    }
+
+    homeVisits(stats){
+        if (!stats) return 0;
+        const result = stats.reduce((acc, call) => Number(call.hv === 1) + acc, 0);
+        if (result === null || isNaN(result) || result === undefined) return 0;
+        return result;
+    }
+
+    lacb(stats){
+        if (!stats) return 0;
+        const result = stats.reduce((acc, call) => Number(call.lacb === 1) + acc, 0);
+        if (result === null || isNaN(result) || result === undefined) return 0;
+        return result;
+    }
+
+    totalCalls(stats, product){
+        if (!stats) return 0;
+        const result = stats.reduce((acc, call) => Number(call.product === product) + acc, 0);
+        if (result === null || isNaN(result) || result === undefined) return 0;
+        return result;
+    }
+
+    opportunities(stats, product){
+        if (!stats) return 0;
+        const result = stats.reduce((acc, call) => Number(call.opportunity === 1 && call.product === product) + acc, 0);
+        if (result === null || isNaN(result) || result === undefined) return 0;
+        return result;
+    }
+
     render() {
         let conversions = this.props.getStats();
-
-        function enrollments(stats, product, callerType){
-            if (!stats) return 0;
-            const result = stats.reduce((acc, call) => Number(call.product === product && call.callerType === callerType && call.enrollment === 1) + acc, 0);
-            if (result === null || isNaN(result) || result === undefined) return 0;
-            return result;
-        }
-
-        function conversionRate(stats, product){
-            if (!stats) return 0;
-            const result = Number(stats.reduce((acc, call) => Number(call.product === product /*&& call.callerType === 'P'*/ && (call.hv === 1 || call.lacb || call.enrollment)) + acc, 0) /
-                stats.reduce((acc, call) => Number(call.product === product /*&& call.callerType === 'P'*/ && call.opportunity === 1) + acc, 0) * 100).toFixed(2)
-            if (result === null || isNaN(result) || result === undefined) return 0;
-            return result;
-        }
-
-        function homeVisits(stats){
-            if (!stats) return 0;
-            const result = stats.reduce((acc, call) => Number(call.hv === 1) + acc, 0);
-            if (result === null || isNaN(result) || result === undefined) return 0;
-            return result;
-        }
-
-        function lacb(stats){
-            if (!stats) return 0;
-            const result = stats.reduce((acc, call) => Number(call.lacb === 1) + acc, 0);
-            if (result === null || isNaN(result) || result === undefined) return 0;
-            return result;
-        }
-
-        function totalCalls(stats, product){
-            if (!stats) return 0;
-            const result = stats.reduce((acc, call) => Number(call.product === product) + acc, 0);
-            if (result === null || isNaN(result) || result === undefined) return 0;
-            return result;
-        }
-
-        function opportunities(stats, product){
-            if (!stats) return 0;
-            const result = stats.reduce((acc, call) => Number(call.opportunity === 1 && call.product === product) + acc, 0);
-            if (result === null || isNaN(result) || result === undefined) return 0;
-            return result;
-        }
+        const self = this;
 
         function stats(attr) {
             return (
                 <tr>
                     <th scope="row">{attr}</th>
                     <td className="MAL">
-                        { totalCalls(conversions[attr],     'MA')       }</td>
-                    <td>{ opportunities(conversions[attr],  'MA')       }</td>
-                    <td>{ enrollments(conversions[attr],    'MA', 'P')  }</td>
-                    <td>{ enrollments(conversions[attr],    'MA', 'M')  }</td>
-                    <td>{ homeVisits(conversions[attr])                  }</td>
-                    <td>{ lacb(conversions[attr])                        }</td>
+                        { self.totalCalls(conversions[attr],     'MA')       }</td>
+                    <td>{ self.opportunities(conversions[attr],  'MA')       }</td>
+                    <td>{ self.enrollments(conversions[attr],    'MA', 'P')  }</td>
+                    <td>{ self.enrollments(conversions[attr],    'MA', 'M')  }</td>
+                    <td>{ self.homeVisits(conversions[attr])                 }</td>
+                    <td>{ self.lacb(conversions[attr])                       }</td>
+                    <td>{ self.rawConversionRate(conversions[attr], 'MA')    }</td>
                     <td className="MAR">
-                        { conversionRate(conversions[attr], 'MA')       }</td>
-                    <td>{ totalCalls(conversions[attr],     'PDP')      }</td>
-                    <td>{ opportunities(conversions[attr],  'PDP')      }</td>
-                    <td>{ enrollments(conversions[attr],    'PDP', 'P') }</td>
-                    <td>{ enrollments(conversions[attr],    'PDP', 'M') }</td>
-                    <td>{ conversionRate(conversions[attr], 'PDP')      }</td>
+                        { self.conversionRate(conversions[attr], 'MA')       }</td>
+                    <td>{ self.totalCalls(conversions[attr],     'PDP')      }</td>
+                    <td>{ self.opportunities(conversions[attr],  'PDP')      }</td>
+                    <td>{ self.enrollments(conversions[attr],    'PDP', 'P') }</td>
+                    <td>{ self.enrollments(conversions[attr],    'PDP', 'M') }</td>
+                    <td>{ self.rawConversionRate(conversions[attr], 'PDP')   }</td>
+                    <td>{ self.conversionRate(conversions[attr], 'PDP')      }</td>
                 </tr>
             );
         }
@@ -87,23 +111,25 @@ class Stats extends Component {
                     <thead>
                     <tr>
                         <td></td>
-                        <td colSpan="7">MA</td>
-                        <td colSpan="5">PDP</td>
+                        <td colSpan="8">MA</td>
+                        <td colSpan="6">PDP</td>
                     </tr>
                     <tr>
                         <th></th>
-                        <th className="MAL">Calls</th>
-                        <th>Opportunities</th>
-                        <th>NE</th>
-                        <th>PC</th>
-                        <th>HV</th>
-                        <th>LACB</th>
-                        <th className="MAR">Conversion %</th>
-                        <th>Calls</th>
-                        <th>Opportunities</th>
-                        <th>NE</th>
-                        <th>PC</th>
-                        <th>Conversion %</th>
+                        <th className="MAL" title="Total Calls">Calls</th>
+                        <th title="Calls with opportunity of conversion">Opportunities</th>
+                        <th title="New Enrollments of prospects">NE</th>
+                        <th title="Plan Change for current members">PC</th>
+                        <th title="Home Visits">HV</th>
+                        <th title="Licensed Agent Callback">LACB</th>
+                        <th title="Raw Conversion Rate - all enrollments and leads divided by total opportunities">Raw Conv %</th>
+                        <th className="MAR" title="Conversion Rate - only new enrollments over opportunities">Conv %</th>
+                        <th title="Total Calls">Calls</th>
+                        <th title="Calls with opportunity of conversion">Opportunities</th>
+                        <th title="New Enrollments of prospects">NE</th>
+                        <th title="Plan Change for current members">PC</th>
+                        <th title="Raw Conversion Rate - all enrollments and leads divided by total opportunities">Raw Conv %</th>
+                        <th title="Conversion Rate - only new enrollments over opportunities">Conversion %</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -122,22 +148,23 @@ class Stats extends Component {
 
         const generalStats = (this.props.getLoggedIn()) ? (
                 <div id="generalStats" className="container row">
-                    <div id="siteGoal" className="offset-3 col-3 text-center row">
+                    <div id="siteGoal" className="offset-3 col-3 row" title="Your progress to reaching your site's goal for each agent during AEP">
                         <div id="siteGoalBreakdown" className="col-8 row">
                             <span className="col-6">MANE:</span>
-                            <span className="col-6">{ enrollments(conversions.aepToDate, 'MA', 'P') }</span>
+                            <span className="col-6">{ this.enrollments(conversions.aepToDate, 'MA', 'P') }</span>
                             <br />
                             <span className="col-6">Goal:</span>
-                            <span className="col-6">{ this.state.siteGoal[sessionStorage.getItem('client')] }</span>
+                            <span className="col-6">{ this.state.siteGoal[sessionStorage.getItem('client')][sessionStorage.getItem('site')] }</span>
                         </div>
                         <span id="siteGoalPercent" className="col-4">
-                            { Number(enrollments(conversions.aepToDate, 'MA', 'P') / this.state.siteGoal[sessionStorage.getItem('client')] * 100).toFixed(2)}%
+                            { Number(this.enrollments(conversions.aepToDate, 'MA', 'P') /
+                                this.state.siteGoal[sessionStorage.getItem('client')][sessionStorage.getItem('site')] * 100).toFixed(2)}%
                         </span>
                     </div>
-                    <div id="gpEntries" className="offset-1 col-5 text-center">
+                    <div id="gpEntries" className="offset-1 col-5" title="Entries into Grand Prize raffle - 1 for every 5 MANE enrollments">
                         Grand Prize Entries: { Math.floor(
-                            ( enrollments(conversions.aepToDate, 'MA', 'P')
-                                + enrollments(conversions.aepToDate, 'PDP', 'P') )
+                            ( this.enrollments(conversions.aepToDate, 'MA', 'P')
+                                + this.enrollments(conversions.aepToDate, 'PDP', 'P') )
                             / 5 // Every 5 new enrollments gives an entry to the gp drawing
                         ) }
                     </div>
